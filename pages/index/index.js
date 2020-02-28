7c8654fa8c0c53e482bc7b73038755bde6ba0a93
xiexuan666 <3025366595@qq.com>
2020-02-28 21:22:07 +0800

    楼梯层和顶部完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,21 +4,49 @@ Page({
     //轮播图的数据
     banners: [],
     //菜单栏的数据
-    menus:[]
+    menus:[],
+    //楼层
+    floors:[],
+    //是否回到顶部
+    isShowTop:[]
   },
   onLoad() {
     //请求轮播图接口
     request({
       url: "/home/swiperdata"
     }).then(res => {
-      console.log(res)
       //message是轮播图的数组
       const { message } = res.data;
-      console.log(message)
       //赋值给banners
       this.setData({
         banners: message
       })
+    })
+    //请求中间导航接口
+    request({
+      url:"/home/catitems"
+    }).then(res => {
+      const {message}  = res.data;
+      this.setData({
+        menus:message
+      })
+    })
+    //请求楼梯层
+    request({
+      url:"/home/floordata"
+    }).then(res => {
+      console.log(res)
+      const {message} = res.data;
+      this.setData({
+        floors:message
+      });
+    })
+  },
+  //小程序回到顶部
+  handleToTop(){
+    wx.pageScrollTo({
+      scrollTop: 0,
+      duration:300
     })
   },
 })
