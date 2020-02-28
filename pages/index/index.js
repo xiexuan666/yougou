85804754b05a54f0d2c70c38d84303c8d27769f9
xiexuan666 <3025366595@qq.com>
2020-02-28 20:19:47 +0800

    首页和轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,24 @@
+import request from "../../utils/request.js";
 Page({
-  onLoad: function () {
-    
-  }
+  data: {
+    //轮播图的数据
+    banners: [],
+    //菜单栏的数据
+    menus:[]
+  },
+  onLoad() {
+    //请求轮播图接口
+    request({
+      url: "/home/swiperdata"
+    }).then(res => {
+      console.log(res)
+      //message是轮播图的数组
+      const { message } = res.data;
+      console.log(message)
+      //赋值给banners
+      this.setData({
+        banners: message
+      })
+    })
+  },
 })
