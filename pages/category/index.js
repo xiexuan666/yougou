f95f74c6467ff374e0ffe299c1dba62897756651
xiexuan666 <3025366595@qq.com>
2020-03-01 19:06:41 +0800

    分类完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,33 @@
-// pages/category/index.js
+import request from "../../utils/request.js"
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    current:0,
+    list:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //监听页面加载
+  onLoad:function (options){
+    //请求分类页的数据
+    request({
+      url:"/categories"
+    }).then(res=>{
+      //把数据列表保存到data
+      const {message} = res.data;
+      this.setData({
+        list:message
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  //点击左边的菜单栏的时候触发
+  handleClick(e){
+    // index参数
+    const {index} = e.currentTarget.dataset;
+    //保存到当前点击的索引到data
+    this.setData({
+      current:index
+    })
+  } 
 })
