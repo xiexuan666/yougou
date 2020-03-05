08612465a2a77ab1462e7a22bd4d4809fa92fef0
xiexuan666 <3025366595@qq.com>
2020-03-05 16:53:02 +0800

    商品详情页完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,19 +1,63 @@
-// pages/goods_detail/index.js
+import request from "../../utils/request.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  banners:[
-    "https://img14.360buyimg.com/babel/s1180x940_jfs/t1/95536/21/13800/74692/5e5dd0e1E4437cd3a/3471b54be70e5189.jpg.webp"
-    
-    
-  ]
+    //商品的详情
+    detail: {},
+    //记录tab栏当前的索引
+    current: 0,
+    //预览图片
+    picUrls: []
   },
-   //监听页面加载
+  //监听页面加载
   onLoad: function (options) {
+    console.log(options)
+    request({
+      url: "/goods/detail",
+      data: {
+        goods_id: options.id
+      }
+    }).then(res => {
+      const { message } = res.data;
+      //获取图片
+      const picUrls = message.pics.map(v => {
+        return v.pics_big
+      });
+      this.setData({
+        detail: message,
+        picUrls   //预览图片接口的使用
+      })
+    })
+  },
 
+  //商品详情的tab切换
+  handleTab(e) {
+    const { index } = e.currentTarget.dataset;
+
+    this.setData({
+      current: index
+    })
+  },
+
+  //预览图片
+  handlePice(e) {
+    //获取当前点击的图片的索引值
+    const { index } = e.currentTarget.dataset;
+
+    wx.previewImage({
+      //当前显示的图片
+      current: this.data.picUrls[index],
+      //预览图片的http链接
+      urls: this.data.picUrls,
+    })
+  },
+  //跳转到购物车页面
+  hanleCart() {
+    wx.switchTab({
+      url: '/pages/cart/index'
+    })
   }
- 
 })
