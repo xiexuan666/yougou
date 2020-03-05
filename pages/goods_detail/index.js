d5a84ae8cc2b51ddc1f9427be7fbd1fb24fc7fe2
xiexuan666 <3025366595@qq.com>
2020-03-05 15:39:46 +0800

    商品详情页的静态布局完成

---
+++ pages/goods_detail/index.js
@@ -0,0 +1,19 @@
+// pages/goods_detail/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  banners:[
+    "https://img14.360buyimg.com/babel/s1180x940_jfs/t1/95536/21/13800/74692/5e5dd0e1E4437cd3a/3471b54be70e5189.jpg.webp"
+    
+    
+  ]
+  },
+   //监听页面加载
+  onLoad: function (options) {
+
+  }
+ 
+})
